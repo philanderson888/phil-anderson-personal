746c85442275f69168cc2af5ffdcc8babeb217d3
philanderson888 <philanderson888@hotmail.com>
2025-03-16 13:50:19 +0000

    add new routes to portfolio archive

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { Home } from './pages/Home';
 import { Portfolio } from './pages/Portfolio';
+import { Archive } from './pages/Archive.tsx';
 import { Contact } from './pages/Contact';
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/portfolio/:id" element={<Portfolio />} />
+            <Route path="/archive" element={<Archive />} />
+            <Route path="/archive/:id" element={<Archive />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
