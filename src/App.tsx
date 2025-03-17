23b17e581e4c8c7d744d665302fe148b257081ef
philanderson888 <philanderson888@hotmail.com>
2025-03-17 15:56:17 +0000

    add 'about' route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { Home } from './pages/Home';
+import { About } from './pages/About';
 import { Portfolio } from './pages/Portfolio';
 import { Archive } from './pages/Archive.tsx';
 import { Contact } from './pages/Contact';
@@ -14,6 +15,7 @@ function App() {
         <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
             <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/portfolio/:id" element={<Portfolio />} />
             <Route path="/archive" element={<Archive />} />
@@ -26,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
